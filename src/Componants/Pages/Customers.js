63135e96b9455ad1611ae19b58df222ec305d126
BGwaltney622 <BGwaltney622@yahoo.com>
2020-11-20 20:16:28 -0600

    price and photo update

--- src/Componants/Pages/Customers.js
+++ src/Componants/Pages/Customers.js
@@ -11,15 +11,17 @@ function Customers() {
                     <h3>Services and Prices</h3>
                     <ul>
                         <li>Mini Facial - $20</li>
-                        <li>Facial - $30*</li>
+                        <li>Facial - $25*</li>
                         <p>Mini Facial/Facial Add-ons:
                             <ul>
-                                <li>Microdermabrasion - $25</li>
+                                <li>Microdermabrasion - $15</li>
                                 <li>Galvanic Treatment - $15</li>
-                                <li>High Frequency - $15</li>
-                                <li>Vacuum Pores - $15</li>
-                                <li>Micro-current - $20</li>
-                                <li>LED Light Therapy - $20</li>
+                                <li>High Frequency - $10</li>
+                                <li>Vacuum Pores - $10</li>
+                                <li>Micro-current - $15</li>
+                                <li>LED Light Therapy - $10</li>
+                                <li>Facial Cupping - $10</li>
+                                <li>Jelly Mask - $10</li>
                             </ul>
                         </p>
                         <li>Waxing:
@@ -27,26 +29,28 @@ function Customers() {
                                 <li>Lip - $6</li>
                                 <li>Brow - $6</li>
                                 <li>Chin - $6</li>
+                                <li>Nose - $6</li>
                                 <li>Arms - $20*</li>
                                 <li>Under Arms - $20*</li>
                                 <li>Lower Legs - $20*</li>
                                 <li>Full Legs - $35*</li>
-                                <li>Back/Chest - $30*</li>
+                                <li>Back/Chest - $20*</li>
                             </ul>
                         </li>
-                        <li>Back Treatments - $30*</li>
+                        <li>Back Treatments - $25*</li>
                         <li>Make Up Application - $10</li>
+                        <li>Ear Candling - $15</li>
                         <li>Brow or Lash Tint - $15 for One or $25 for Both</li>
-                        <li>Lash Lift - $30</li>
-                        <li>Lash Extensions - $50*</li>
+                        <li>Lash Lift - $25</li>
+                        <li>Lash Extensions - $40*</li>
                         <p>* Prices Starting At</p>
                     </ul>
                 </div>
                 <div className="p-2" style={iCourseStyle}>
                     <h4>Seasonal Facials</h4>
-                    <img src="TropicalBeachFacial.jpg" className="img-fluid" alt='' style={facialStyle}/>
-                    <img src="CherryBlossomFacial.jpg" className="img-fluid" alt='' style={facialStyle}/>
-                    <img src="TropicalCocounutFacial.jpg" className="img-fluid" alt='' style={facialStyle}/>
+                    <img src="Pumpkin.png" className="img-fluid" alt='' style={facialStyle}/>
+                    <img src="Black_Cherry.png" className="img-fluid" alt='' style={facialStyle}/>
+                    <img src="Charcoal Purity Facial 4x6.jpg" className="img-fluid" alt='' style={facialStyle}/>
                     <h5 style= {contactStyle}>To Schedule Appointments Contact Us At:</h5>
                     <h5>Phone: (214) 903-7318</h5>
                 </div>
